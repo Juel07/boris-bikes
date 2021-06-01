d9d5a6a84b974abba542c443bd7d014ecf68afa8
Emily <emily.fitzsimmonsx@gmail.com>
2021-06-01 17:10:14 +0100

    chapter 7

--- spec/dockingstation_spec.rb
+++ spec/dockingstation_spec.rb
@@ -1,4 +1,8 @@
-require 'dockingstation'
+require 'dockingstation.rb'
 describe DockingStation do
+  it 'expects DockingStation instances to respond_to the method release_bike' do
+  respond_to = DockingStation.new
+  respond_to.release_bike
+end
   
 end
